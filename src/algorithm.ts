9f13615af386dba4306a734f5bc639bbe81e030a
atishay11jain <88791784+atishay11jain@users.noreply.github.com>
2025-07-21 05:28:23 +0000

    sliding window algorithm implemented

--- src/algorithm.ts
+++ src/algorithm.ts
@@ -31,3 +31,25 @@ export class FixedWindowAlgorithm extends BaseRateLimitAlgorithm {
     }
   }
 }
+
+export class SlidingWindowAlgorithm extends BaseRateLimitAlgorithm {
+  async checkRateLimit(key: string): Promise<RateLimitResult> {
+    try {
+      const currentTime = Date.now();
+      const windowStart = currentTime - this.windowSizeInSeconds * 1000;
+      const sortedSetKey = `${key}:sliding_window`;
+
+      await this.redisStore.removeFromSortedSet(sortedSetKey, 0, windowStart);
+      const currentCount = await this.redisStore.getSortedSetCount(sortedSetKey);
+      const allowed = currentCount < this.maxRequests;
+      if(allowed){
+        await this.redisStore.addToSortedSet(sortedSetKey, currentTime, JSON.stringify(currentTime), this.windowSizeInSeconds);
+      }
+      const remainingRequests = Math.max(0, this.maxRequests - currentCount);
+      return { allowed, remainingRequests };
+    } catch (error) {
+      console.error("SlidingWindowAlgorithm error:", error);
+      return { allowed: true, remainingRequests: this.maxRequests - 1 };
+    }
+  }
+}
